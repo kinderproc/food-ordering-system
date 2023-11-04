09c059f830cafe9138cba6098260556745738e54
kinderproc <kinderproc84@yandex.ru>
2023-11-04 22:48:38 +0300

    tracking order logic implemented

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/OrderTrackCommandHandler.java
@@ -2,14 +2,35 @@ package com.food.ordering.system.order.service.domain.ports;
 
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class OrderTrackCommandHandler {
+    private final OrderRepository orderRepository;
+    private final OrderDataMapper orderDataMapper;
+
+    public OrderTrackCommandHandler(OrderRepository orderRepository, OrderDataMapper orderDataMapper) {
+        this.orderRepository = orderRepository;
+        this.orderDataMapper = orderDataMapper;
+    }
 
     public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
-        return TrackOrderResponse.builder().build();
+        Optional<Order> orderResult =
+                orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()));
+        if (orderResult.isEmpty()) {
+            log.warn("Could not find order with tracking id: {}", trackOrderQuery.getOrderTrackingId());
+            throw new OrderNotFoundException("Could not find order with tracking id: " +
+                    trackOrderQuery.getOrderTrackingId());
+        }
+        return orderDataMapper.orderToTrackOrderResponse(orderResult.get());
     }
 }
