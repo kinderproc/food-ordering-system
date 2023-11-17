70c114c9f651e4c74cdff4b915a593917ad57c14
kinderproc <kinderproc84@yandex.ru>
2023-11-17 10:23:56 +0300

    restaurant data access implemented

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/restaurant/exception/RestaurantEntityException.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.order.service.dataaccess.restaurant.exception;
+
+public class RestaurantEntityException extends RuntimeException {
+
+    public RestaurantEntityException(String message) {
+        super(message);
+    }
+}
